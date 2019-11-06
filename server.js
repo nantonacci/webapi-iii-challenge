465ef1ba14d9b7628913556481ca5fbdceaad58c
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-11-05 18:43:36 -0800

    Basic setup and  middleware added.

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = 'express';
-
+const express = require('express');
+const userRouter = require('./users/userRouter.js');
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
+  next();
+}
 
-};
+server.use(logger);
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,80 @@
-const express = 'express';
+const express = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', (req, res) => {});
 
-});
+router.post('/:id/posts', (req, res) => {});
 
-router.post('/:id/posts', (req, res) => {
+router.get('/', (req, res) => {});
 
-});
+router.get('/:id', (req, res) => {});
 
-router.get('/', (req, res) => {
+router.get('/:id/posts', (req, res) => {});
 
-});
+router.delete('/:id', (req, res) => {});
 
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put('/:id', (req, res) => {});
 
 //custom middleware
 
+// READY TO TEST
 function validateUserId(req, res, next) {
+  const { id } = req.params;
 
-};
+  User.getById(id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).json({ message: 'invalid user id' });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'exception' }, err);
+    });
+}
 
+// READY TO TEST
 function validateUser(req, res, next) {
+  const { body } = req.params;
 
-};
+  User.get(body)
+    .then(user => {
+      if (!body) {
+        res.status(400).json({ message: 'missing user data' });
+      }
+      if (!name) {
+        res.status(400).json({ message: 'missing required name field' });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'exception', err });
+    });
+  next();
+}
 
+// READY TO TEST
 function validatePost(req, res, next) {
+  const { body } = req.params;
 
-};
+  Post.get(body)
+    .then(user => {
+      if (!body) {
+        res.status(400).json({ message: 'missing post data' });
+      }
+      if (!text) {
+        res.status(400).json({ message: 'missing required text field' });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'exception', err });
+    });
+  next();
+}
 
 module.exports = router;
